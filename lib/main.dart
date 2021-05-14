f60d513562a42a8d8f5b19207733154e9a0b5368
Kaushal Makadia <kaushalmakadia123@gmail.com>
2021-05-15 01:02:41 +0530

    name_change

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
                 height: 5.0,
               ),
               Text(
-                'Kaushal Makadia',
+                'Kaushal',
                 style: TextStyle(
                   fontFamily: 'AkayaTelivigala',
                   fontSize: 40.0,
